0e3a312ab9ec9ba6087474c0f5a8cda52b66a77a
brettmadrid <brettmadrid9@gmail.com>
2019-12-30 22:53:16 -0800

    added BFS, DFSPreOrder, DFSPostOrder, DFSInOrder

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -8,34 +8,105 @@ class Node {
 
 class BinarySearchTree {
   constructor() {
-    this.node = null;
+    this.root = null;
   }
-
-  insert(val) {
-    const newNode = new Node(val);
-
-    if (!this.node) {
-      this.node = newNode;
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
       return this;
-    } else {
-      let current = this.node;
-      while (true) {
-        if (val < current.value) {
-          if (!current.left) {
-            current.left = newNode;
-            return this;
-          } else {
-            current = current.left;
-          }
-        } else {
-          if (!current.right) {
-            current.right = newNode;
-            return this;
-          } else {
-            current = current.right;
-          }
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
         }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
       }
     }
+  }
+  find(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  BFS() {
+    var node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() {
+    var data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSPostOrder() {
+    var data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSInOrder() {
+    var data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
   }
 }
